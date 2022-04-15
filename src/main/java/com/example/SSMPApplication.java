4928293463ed6db741c4f599754bfe260705b13f
shanshanlaila <75969328+shanshanlaila@users.noreply.github.com>
2022-04-15 20:41:58 +0800

    Update SSMPApplication.java

--- src/main/java/com/example/SSMPApplication.java
+++ src/main/java/com/example/SSMPApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class SSMPApplication {
 
     public static void main(String[] args) {
+        //更新了一个注解
         SpringApplication.run(SSMPApplication.class, args);
     }
 
